c3902a995f374857a8ccef3c928fb5ed71abf99b
Sofiya Kunitsyna <31761683+Kunica97@users.noreply.github.com>
2018-04-15 12:19:14 +0300

    Delete index.js

--- bundlejs/index.js
+++
@@ -1,6 +0,0 @@
-let moment = require("moment");
-let faker = require("faker");
-
-let pre = document.createElement('pre');
-pre.innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a') + '<br>' + faker.internet.email();
-document.body.appendChild(pre);
